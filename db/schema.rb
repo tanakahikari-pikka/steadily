004e0bd0c9c941bd8a66925792f6ca9baad947de
田中光 <hikari@tanakahikarinoMacBook-Pro.local>
2022-07-16 14:05:34 +0900

    ログイン前後のページ制限、usersにname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_164052) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_16_044832) do
+  create_table "logs", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "study_record", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_164052) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
